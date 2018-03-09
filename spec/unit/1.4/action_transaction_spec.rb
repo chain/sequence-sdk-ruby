e20cc2f60703d662e5d29f9d8c0c01c222f527ee
Dan Croak <dan@statusok.com>
2018-03-09 01:06:19 +0000

    sdk/ruby: remove Sequence::Transaction::Action
    This class is used to "hydrate" the JSON response from transaction
    endpoints. Any class that defines the correct attributes can be used.
    
    We have another class, `Sequence::Action`, that is identical.
    Removing `Sequence::Transaction::Action` "just works" because existing
    references to `Action` will walk the class hierarchy and find
    `Sequence::Action` now.
    
    This may help us avoid inconsistencies or mistakes trying to manage two
    of the same class.
    
    Closes #2695
    
    Author: Dan Croak <dan@statusok.com>
    Date: Thu Mar 8 17:02:18 2018 -0800
    upstream:587b1728cbe6f144c5202f92d56fa6885e788bd5

--- spec/unit/1.4/action_transaction_spec.rb
+++ spec/unit/1.4/action_transaction_spec.rb
@@ -1,67 +1,65 @@
-[Sequence::Action, Sequence::Transaction::Action].each do |class_under_test|
-  describe class_under_test do
-    describe '#snapshot' do
-      it 'is Hash-like' do
-        action = described_class.new(
-          snapshot: {
-            'action_tags' => action_tags,
-            'destination_account_tags' => destination_account_tags,
-            'flavor_tags' => flavor_tags,
-            'source_account_tags' => source_account_tags,
-            'token_tags' => token_tags,
-          },
-        )
+describe Sequence::Action do
+  describe '#snapshot' do
+    it 'is Hash-like' do
+      action = described_class.new(
+        snapshot: {
+          'action_tags' => action_tags,
+          'destination_account_tags' => destination_account_tags,
+          'flavor_tags' => flavor_tags,
+          'source_account_tags' => source_account_tags,
+          'token_tags' => token_tags,
+        },
+      )
 
-        snapshot = action.snapshot
+      snapshot = action.snapshot
 
-        expect(snapshot['action_tags']).to eq(action_tags)
-        expect(snapshot['destination_account_tags'])
-          .to eq(destination_account_tags)
-        expect(snapshot['flavor_tags']).to eq(flavor_tags)
-        expect(snapshot['source_account_tags']).to eq(source_account_tags)
-        expect(snapshot['token_tags']).to eq(token_tags)
-      end
-
-      it 'can access tags via dot notation' do
-        action = described_class.new(
-          snapshot: {
-            'action_tags' => action_tags,
-            'destination_account_tags' => destination_account_tags,
-            'flavor_tags' => flavor_tags,
-            'source_account_tags' => source_account_tags,
-            'token_tags' => token_tags,
-          },
-        )
-
-        snapshot = action.snapshot
-
-        expect(snapshot.action_tags).to eq(action_tags)
-        expect(snapshot.destination_account_tags)
-          .to eq(destination_account_tags)
-        expect(snapshot.flavor_tags).to eq(flavor_tags)
-        expect(snapshot.source_account_tags).to eq(source_account_tags)
-        expect(snapshot.token_tags).to eq(token_tags)
-      end
+      expect(snapshot['action_tags']).to eq(action_tags)
+      expect(snapshot['destination_account_tags'])
+        .to eq(destination_account_tags)
+      expect(snapshot['flavor_tags']).to eq(flavor_tags)
+      expect(snapshot['source_account_tags']).to eq(source_account_tags)
+      expect(snapshot['token_tags']).to eq(token_tags)
     end
 
-    def action_tags
-      { 'due' => 'january-1' }
-    end
+    it 'can access tags via dot notation' do
+      action = described_class.new(
+        snapshot: {
+          'action_tags' => action_tags,
+          'destination_account_tags' => destination_account_tags,
+          'flavor_tags' => flavor_tags,
+          'source_account_tags' => source_account_tags,
+          'token_tags' => token_tags,
+        },
+      )
 
-    def destination_account_tags
-      { 'bank' => 'first-republic' }
-    end
+      snapshot = action.snapshot
 
-    def flavor_tags
-      { 'mint' => 'san-francisco' }
+      expect(snapshot.action_tags).to eq(action_tags)
+      expect(snapshot.destination_account_tags)
+        .to eq(destination_account_tags)
+      expect(snapshot.flavor_tags).to eq(flavor_tags)
+      expect(snapshot.source_account_tags).to eq(source_account_tags)
+      expect(snapshot.token_tags).to eq(token_tags)
     end
+  end
 
-    def source_account_tags
-      { 'bank' => 'td-bank' }
-    end
+  def action_tags
+    { 'due' => 'january-1' }
+  end
 
-    def token_tags
-      { 'currency' => 'usd' }
-    end
+  def destination_account_tags
+    { 'bank' => 'first-republic' }
+  end
+
+  def flavor_tags
+    { 'mint' => 'san-francisco' }
+  end
+
+  def source_account_tags
+    { 'bank' => 'td-bank' }
+  end
+
+  def token_tags
+    { 'currency' => 'usd' }
   end
 end
